a333e2f8f673fcbd42adc22904bf70c124a426cf
Long Pham <famthanhlong@gmail.com>
2017-07-28 16:15:48 +0900

    Completed the first prototype.

---
+++ DotPlot.py
@@ -0,0 +1,43 @@
+from tkinter import *
+from tkinter import ttk
+import pylab
+
+def alignSequences():
+    """Create a dot plot of the two DNA sequences specified by the user"""
+    window = 1
+    seq_one = firstSeq.get()
+    seq_two = secondSeq.get()
+    data = [[(seq_one[i:i + window] != seq_two[j:j + window]) \
+        for j in range(len(seq_one) - window + 1)] \
+        for i in range(len(seq_two) - window + 1)]
+    pylab.gray()
+    pylab.imshow(data)
+    pylab.xlabel("%s (length %i bp)" % ("First sequence", len(seq_one)))
+    pylab.ylabel("%s (length %i bp)" % ("Second sequence", len(seq_two)))
+    pylab.title("Dot plot using window size %i" % window)
+    pylab.show()
+
+root = Tk()
+root.title ('Bioinformatics Dot Plot')
+frame = ttk.Frame(root)
+
+firstSeq = StringVar()
+secondSeq = StringVar()
+
+# Widgests in the control frame
+label1 = ttk.Label(frame, text = 'Specify two DNA sequneces with bases A, T, G, and C')
+label2 = ttk.Label(frame, text = 'First sequence')
+label3 = ttk.Label(frame, text = 'Second sequence')
+entry1 = ttk.Entry(frame, textvariable = firstSeq)
+entry2 = ttk.Entry(frame, textvariable = secondSeq)
+button = ttk.Button(frame, text = 'Dot plot', command = alignSequences)
+
+frame.grid(column = 0, row = 0)
+label1.grid(column = 0, row = 0, columnspan = 2)
+label2.grid(column = 0, row = 1)
+label3.grid(column = 0, row = 2)
+entry1.grid(column = 1, row = 1)
+entry2.grid(column = 1, row = 2)
+button.grid(column = 2, row = 2)
+
+root.mainloop()
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Biological-Sequences-Dot-Plot
+
+### Overview
+This application allows users to input two DNA sequences and displays a dot matrix of these sequences.
+
+Dot matrix analysis is one approach to comparing biological sequences. Dot matrix analysis works by aligning two input sequences on axes and placing dots wherever matches of symbols (i.e. DNA bases) are found. Although this approach relies on human visual analysis and does not have mathematical rigour, a dot matrix analysis can give us a clue about the existence of potential sequence alignments. Hence, it is oftentimes applied prior to sequence alignment algorithms. 
+
+### Installation
+To run this application, you need to install Python together with two additional libraries: Biopython and Matplotlib. The detail of these packages, including how to install them, can be found [here](http://biopython.org/wiki/Biopython) and [here](https://matplotlib.org/index.html).
